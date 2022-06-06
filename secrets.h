ff5faa21280d49ae4096c67992a4df4a99891b96
Jair Huaman Bellido <huamanbellidoj@gmail.com>
2022-06-06 16:51:31 -0500

    Create secrets.h

---
+++ secrets.h
@@ -0,0 +1,27 @@
+#define SECRET
+#define THINGNAME "THINGNAME"
+
+const char WIFI_SSID[] = "<YOUR_SSID>";
+const char WIFI_PASSWORD[] = "<YOUR_WIFI_PASSWORD>";
+const char AWS_IOT_ENDPOINT[] = "<YOUR_AWS_IOT_ENDPOINT>";
+
+// Amazon Root CA 1
+static const char AWS_CERT_CA[] PROGMEM = R"EOF(
+-----BEGIN CERTIFICATE-----
+
+-----END CERTIFICATE-----
+)EOF";
+
+// Device Certificate
+static const char AWS_CERT_CRT[] PROGMEM = R"KEY(
+-----BEGIN CERTIFICATE-----
+
+  -----END CERTIFICATE-----
+)KEY";
+
+// Device Private Key
+static const char AWS_CERT_PRIVATE[] PROGMEM = R"KEY(
+-----BEGIN RSA PRIVATE KEY-----
+
+-----END RSA PRIVATE KEY-----
+)KEY";
